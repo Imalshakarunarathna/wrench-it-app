0cae717e93f3b0365872a424718eb01050a42dc9
Imalshakarunarathna <143775743+Imalshakarunarathna@users.noreply.github.com>
2024-04-25 08:46:22 +0530

    Update Feedback.js

--- web/src/components/Feedback.js
+++ web/src/components/Feedback.js
@@ -4,19 +4,7 @@ import "../styles/Feedback.css";
 export default function Feedback() {
   return (
     <div className="feedback-container">
-      <div className="giphy-container">
-        <iframe
-          src="https://giphy.com/embed/oNr6IBi0Hnnnq"
-          width="399"
-          height="480"
-          frameBorder="0"
-          className="giphy-embed"
-          allowFullScreen
-        ></iframe>
-        <p>
-          <a href="https://giphy.com/gifs/dancing-kermit-etOX3h7ApZuDe7Fc5w"></a>
-        </p>
-  </div> 
+    
     </div>
   );
 }
